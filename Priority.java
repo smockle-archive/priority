5b7ee937598fb55e01f7d78f74cb8eda3ef127de
Clay Miller <clay@smockle.com>
2013-07-25 20:33:10 -0500

    Begin implementing driver.

---
+++ Priority.java
@@ -0,0 +1,15 @@
+public class Priority {
+  public static void main(String[] args) {
+    // Get Variables
+    Node[] nodes;
+    int size = 0;
+    int n = 0;
+    
+    // Set Variables
+    Scanner scan = new Scanner(System.in);
+    System.out.println("How big is the queue? ");
+    size = scan.nextLine();
+    nodes = new Node[size];
+    scan.close();
+  }
+}
